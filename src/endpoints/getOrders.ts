6d192bb2e009d8013d5b5a4e59afd76e38cdce54
LeonardoTredese <leonardo.tredese@studenti.unipd.it>
2021-05-13 10:38:51 +0200

    fixed get orders

--- src/endpoints/getOrders.ts
+++ src/endpoints/getOrders.ts
@@ -10,16 +10,17 @@ import { ClientConfiguration } from 'aws-sdk/clients/dynamodb';
 import { readFileSync as readFile } from 'fs';
 import getOrders from 'src/lambdas/getOrders';
 import { OrderFilter } from 'src/models/OrderFilters';
+import OrderResponse from 'src/models/OrderResponse';
 
 const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const dynamoConfig: ClientConfiguration = parseDocument(readFile(process.env.DYNAMODB_CONFIG_FILE_PATH, 'utf-8')).toJSON();
   const repo = new OrderRepositoryDynamoDB(new DynamoDB(dynamoConfig));
-  // const userName = event.requestContext.authorizer.claims['cognito:username'] as string
-  // const userGroups = event.requestContext.authorizer.claims['cognito:groups'] as string[]
-  // if (userGroups.includes("buyers"))
+  const userName = event.requestContext.authorizer.claims['cognito:username'] as string;
+  const userGroups = event.requestContext.authorizer.claims['cognito:groups'] as string[];
   const filters: OrderFilter = { ...event.queryStringParameters };
-  return getOrders(repo, filters);
-  // else if (userGroups.includes("sellers"))
+  if (userGroups.includes('buyers')) return getOrders(repo, filters, userName);
+  if (userGroups.includes('sellers')) return getOrders(repo, filters);
+  return new OrderResponse(403);
 };
 
 export default handler;

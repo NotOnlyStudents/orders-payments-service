5a1912f1ff902684cedfffc44f5c8848cbc03ef6
br3sc4 <30345822+br3sc4@users.noreply.github.com>
2021-05-24 10:51:25 +0200

    Add unit tests

--- src/models/OrderFilters.ts
+++ src/models/OrderFilters.ts
@@ -9,7 +9,7 @@ interface OrderFilter {
 
 const emailRegex = /^[-!#$%&'*+/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
 
-function isEmailValid(email) {
+function isEmailValid(email: string) {
   if (!email) return false;
 
   if (email.length > 254) return false;
